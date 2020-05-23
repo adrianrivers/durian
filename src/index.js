b7e8b7581912c1f438ab1686e9b3617b20553193
adrianrivers <adrianlrivers@gmail.com>
2020-05-23 03:13:09 +0200

    Update index, package json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { default as DurianButton } from "./components/button/DurianButton.svelte";
 import { default as DurianCard } from "./components/card/DurianCard.svelte";
-import { default as DurianImage } from "./components/image/Image.svelte";
+import { default as DurianImage } from "./components/image/DurianImage.svelte";
